48a862776a708fcee38a39bc07529b0bb4413796
Travis Banken <travisbanken@TravisBanken-iMac.local>
2019-06-12 11:35:16 -0700

    Added prettier for code format and Constants file

---
+++ src/Constants.js
@@ -0,0 +1,13 @@
+// fonts
+export const CX_FONT = '"Open Sans", "Arial"';
+
+// colors
+export const CX_LIGHT_BLUE = "#00CCFF";
+export const CX_DARK_BLUE = "#00ADD2";
+export const CX_GRAY_BLUE = "#658398";
+export const CX_OFF_WHITE = "#F2F2F2";
+export const LIGHT_BLUE = "#4BC8EE";
+export const DARK_BLUE = "#00ABD4";
+export const GRAY_BLUE = "#506F85";
+export const LIGHT_GRAY = "#477081";
+export const BATMAN_GRAY = "#272727";
