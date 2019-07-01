0ec46e9850b5d084999413a8b4746f290a453316
haydenudelson <haydenudelson18@gmail.com>
2019-07-01 14:17:05 -0700

    Added authentication/log in elements

---
+++ src/GraphConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    appId: '53b3ede2-e972-4d80-8e0d-894151b5ba65',
+    authority: 'https://login.microsoftonline.com/organizations/',
+    scopes: [
+      "user.read",
+      "calendars.read"
+    ]
+  };
