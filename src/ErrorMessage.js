56031020b52275b749777c25657b20a01b011f4e
haydenudelson <haydenudelson18@gmail.com>
2019-07-03 10:54:44 -0700

    Adjusted code using Prettier extension, calendar now generates ALL user events for the month of July, patched bug where Connexta logo disappears

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Alert } from 'reactstrap';
+import React from "react";
+import { Alert } from "reactstrap";
 
 export default class ErrorMessage extends React.Component {
   render() {
     let debug = null;
     if (this.props.debug) {
-      debug = <pre className="alert-pre border bg-light p-2"><code>{this.props.debug}</code></pre>;
+      debug = (
+        <pre className="alert-pre border bg-light p-2">
+          <code>{this.props.debug}</code>
+        </pre>
+      );
     }
     return (
       <Alert color="danger">
