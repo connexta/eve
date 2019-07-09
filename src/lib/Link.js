59a34d0e3781233eff055104eae9ca157f6f566e
Alex Chu <alchucam@gmail.com>
2019-07-09 14:00:55 -0700

    update per requests of PR comments

---
+++ src/lib/Link.js
@@ -0,0 +1,12 @@
+/*AF team*/ 
+
+//Specific AF team Git build pipeline to keep track of
+export const AFpipeline = "SOAESB_Nightly_Release_Builder";
+
+//URL for the AF team main repo to keep track of
+export const AFURL =
+  "http://jenkins.phx.connexta.com/service/jenkins/blue/rest/organizations/jenkins/pipelines/HAART-Jobs/pipelines/SOAESB_Nightly_Release_Builder";
+
+//URL for the AF team main repo Jenkins
+export const AFJenkinLink =
+  "http://jenkins.phx.connexta.com/service/jenkins/job/HAART-Jobs/job/SOAESB_Nightly_Release_Builder";
