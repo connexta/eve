c8f968ffac9a72e037bf06a0c030665cddf804ec
Alex Chu <alchucam@gmail.com>
2019-07-11 11:34:11 -0700

    Modifying fetch data

--- src/BuildStatus.js
+++ src/BuildStatus.js
@@ -3,10 +3,12 @@ import { CX_OFF_WHITE, CX_FONT, BATMAN_GRAY } from "./Constants.js";
 import BuildIcon from "./BuildIcon";
 import Card from "@material-ui/core/Card";
 import { CardHeader, CardContent } from "@material-ui/core";
-import { AllianceJenkinLink } from "./lib/Link";
+import { AllianceURL, DDFURL, DIBURL, GSRURL, AFURL } from "./lib/Link";
 import { extractTime } from "./utilities/utility";
 
-const BUILD_LIST = ["alliance", "ddf", "gsr", "dib"];
+const BUILD_LIST = ["alliance", "ddf", "gsr", "dib", "haart jobs"];
+
+const URL_LIST = [AllianceURL, DDFURL, DIBURL, GSRURL, AFURL];
 
 const URL =
   "http://jenkins.phx.connexta.com/service/jenkins/blue/rest/organizations/jenkins/pipelines/";
@@ -16,18 +18,18 @@ const styles = {
     background: CX_OFF_WHITE,
     fontSize: "50px",
     color: BATMAN_GRAY,
-    "font-family": CX_FONT
+    fontFamily: CX_FONT
   },
   cardheader: {
     background: CX_OFF_WHITE,
     color: BATMAN_GRAY,
-    "font-family": CX_FONT
+    fontFamily: CX_FONT
   },
   cardContent: {
     display: "flex",
-    "flex-direction": "row",
-    "justify-content": "space-between",
-    "flex-wrap": "wrap"
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap"
   }
 };
 
@@ -36,9 +38,9 @@ class BuildStatus extends React.Component {
     super(props);
     this.state = {
       data: [],
-      overviewData: [],
+      fetchedData: [],
       //big dataset wrapped up alliance, etc..
-      allianceData: [],
+      teamData: [],
       isLoading: true
     };
   }
@@ -46,7 +48,7 @@ class BuildStatus extends React.Component {
   // updates the build status every 60 sec
   componentDidMount() {
     this.refreshBuildStatus();
-    this.intervalId = setInterval(() => this.refreshBuildStatus(), 1000 * 10); //NOTICE 60 -> 5
+    this.intervalId = setInterval(() => this.refreshBuildStatus(), 1000 * 10); //NOTICE 60 -> 5   //set to 1 hour or 12 hours..?
   }
 
   componentWillUnmount() {
@@ -54,35 +56,93 @@ class BuildStatus extends React.Component {
   }
 
   refreshBuildStatus() {
-    fetch(URL)
-      .then(response => response.json())
-      .then(jsonData => {
-        this.setState({ overviewData: jsonData});
-      })
-      .catch(e => console.log("error", e));
+    let overallData = [];
+    //fetch initial data such as displayName, weatherScore
+    this.fetchData(URL);
+    overallData = this.setDataInitialInfo();
 
-    fetch(AllianceJenkinLink)
-      .then(response => response.json())
-      .then(jsonData => {
-        this.setState({ allianceData: jsonData});
-      })
-      .catch(e => console.log("error", e));
+    // fetch most recent build time for each team repo
+    for (let i = 0; i < URL_LIST.length; i++){
+      this.fetchData(URL_LIST[i]);
+      this.setDataTimeInfo();
+    }
 
-      this.compactData();
+
+    // fetch(URL)
+    //   .then(response => response.json())
+    //   .then(jsonData => {
+    //     this.setState({ overviewData: jsonData});
+    //   })
+    //   .catch(e => console.log("error", e));
+
+    // fetch(AllianceURL)
+    //   .then(response => response.json())
+    //   .then(jsonData => {
+    //     this.setState({ teamData: jsonData});
+    //   })
+    //   .catch(e => console.log("error", e));
+
+      // this.compactData();
+
   }
 
+  //fetch data from jenkin url
+  async fetchData(URL){
+    await fetch(URL)
+      .then(response => response.json())
+      .then(jsonData => {
+        this.setState({ fetchedData: jsonData});
+        // console.log("inside");
+        // console.log(jsonData);
+      })
+      .catch(e => console.log("error", e));
+
+  }
 
   getMostrecentRun(){
 
   }
 
+  //set data info such as displayName, weatherScore, time (placeholder for now)
+  setDataInitialInfo(){
+    let tempdata = [];
+    let index = 0;
+    if (this.state.fetchedData.length > 0){
+      this.state.fetchedData.map(item => {
+        if (BUILD_LIST.includes(item.displayName.toLowerCase())) {
+          tempdata[index] = {
+            displayName: item.displayName,
+            weatherScore: item.weatherScore,
+            time: ""
+          };
+          index++;
+        }
+      })
+    }
+
+    console.log(tempdata);
+    return tempdata;
+    // this.setState({ data: tempdata });
+
+  }
+
+  setDataTimeInfo(overallData){
+    // for (let i = 0; i < overallData.length; i++){
+    //   // if (overallData.displayName === ){
+
+    //   // }
+    // }
+  }
+
+
+//isLoading: false
  compactData() {
     let tempdata = [];
     let index = 0;
     this.state.overviewData.map(item => {
       if (BUILD_LIST.includes(item.displayName.toLowerCase())) {
         tempdata[index] = {
-          name: item.displayName,
+          displayName: item.displayName,
           weatherScore: item.weatherScore,
           time: ""
         };
@@ -91,7 +151,7 @@ class BuildStatus extends React.Component {
     })
     
     for (let i = 0; i < tempdata.length; i++){
-      tempdata[i].time = "1";
+      tempdata[i].time = extractTime(this.state.teamData[0].startTime);
     }
 
 
@@ -104,34 +164,35 @@ class BuildStatus extends React.Component {
     //   console.log(extractTime(this.state.allianceData[0].startTime));
     // }
     // console.log(this.state.data);
-    console.log(this.state.allianceData);
+    // console.log(this.state.allianceData);
 
     // console.log(this.state.data);
 
     return this.state.isLoading ? (
-      <Card raised="true" style={styles.card}>
+      <Card raised={true} style={styles.card}>
         Loading Build Health. . .
       </Card>
     ) : (
-      <Card raised="true" style={styles.card}>
+      <Card raised={true} style={styles.card}>
         <CardHeader
           title="Build Health"
           style={styles.cardheader}
           titleTypographyProps={{ variant: "h3" }}
         />
         <CardContent style={styles.cardContent}>
-          {this.state.overviewData.map(item => {
-            if (BUILD_LIST.includes(item.displayName.toLowerCase())) {
+          {this.state.data.map(item => {
+            // if (BUILD_LIST.includes(item.displayName.toLowerCase())) {
               return (
                 <BuildIcon
                   score={item.weatherScore}
                   name={item.displayName}
                   key={item.displayName}
-                  time={"11"}
+                  time={item.time}
                 />
               );
-            }
-          })}
+            })
+          // })
+          }
         </CardContent>
       </Card>
     );
--- src/lib/Link.js
+++ src/lib/Link.js
@@ -1,5 +1,15 @@
-/*Alliance*/
+//URL for the Alliance team main repo to keep track of
+export const AllianceURL = "https://jenkins.phx.connexta.com/service/jenkins/blue/rest/organizations/jenkins/pipelines/Alliance/pipelines";
 
-//URL for the Alliance main repo Jenkins
-export const AllianceJenkinLink = "https://jenkins.phx.connexta.com/service/jenkins/blue/rest/organizations/jenkins/pipelines/Alliance/pipelines/master/runs/";
+//URL for the DDF team main repo to keep track of
+export const DDFURL = "https://jenkins.phx.connexta.com/service/jenkins/blue/rest/organizations/jenkins/pipelines/DDF/pipelines/";
+
+//URL for the DIB team main repo to keep track of
+export const DIBURL = "https://jenkins.phx.connexta.com/service/jenkins/blue/rest/organizations/jenkins/pipelines/DIB/pipelines/";
+
+//URL for the GSR team main repo to keep track of
+export const GSRURL = "https://jenkins.phx.connexta.com/service/jenkins/blue/rest/organizations/jenkins/pipelines/GSR/pipelines/";
+
+//URL for the AF team main repo to keep track of
+export const AFURL = "http://jenkins.phx.connexta.com/service/jenkins/blue/rest/organizations/jenkins/pipelines/HAART-Jobs/pipelines/SOAESB_Nightly_Release_Builder/";
 
