7cc04927b07499ba79701e858bb5cd9b0e79d4ca
Alex Chu <alchucam@gmail.com>
2019-07-11 10:08:55 -0700

    refactored listcontents

--- src/BuildAF.js
+++ src/BuildAF.js
@@ -10,12 +10,14 @@ import CardHeader from "@material-ui/core/CardHeader";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import { extractTime } from "./utilities/utility";
+
+const oneHour = 1 * 1000 * 60 * 60;
 
 const styles = {
   card: {
     top: "20px",
     position: "relative",
-    boxShadow: `0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)`,
     background: CX_OFF_WHITE,
     fontFamily: CX_FONT,
     color: BATMAN_GRAY
@@ -34,20 +36,10 @@ const styles = {
   }
 };
 
-//Reformat extracted time for better display
-function extractTime(time) {
-  if (time) {
-    let extractedTime = time.split("T");
-    extractedTime = extractedTime[0] + " " + extractedTime[1].split(".")[0];
-    return extractedTime;
-  }
-}
-
 class BuildAF extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
       isLoading: true,
       failedData: []
     };
@@ -56,12 +48,11 @@ class BuildAF extends React.Component {
   // updates the build status around every midnight with timer checking current hour every 1 hour.
   componentDidMount() {
     this.setState({
-      data: [],
       isLoading: true,
       failedData: []
     });
     this.updateBuildStatus();
-    this.timerIntervalID = setInterval(() => this.timer(), 1 * 1000 * 60 * 60); //1 hour interval
+    this.timerIntervalID = setInterval(() => this.timer(), oneHour); //1 hour interval
   }
 
   componentWillUnmount() {
@@ -77,12 +68,12 @@ class BuildAF extends React.Component {
     }
   }
 
+  //fetch
   updateBuildStatus() {
     fetch(this.props.URL + "runs/")
       .then(response => response.json())
       .then(jsonData => {
         this.setState({
-          data: jsonData,
           isLoading: false,
           failedData: this.getFailedData(jsonData)
         });
@@ -103,6 +94,9 @@ class BuildAF extends React.Component {
     return failedData;
   }
 
+  //@return:
+  //  icon of success or failure depends on the result
+  //  followed by description of data content
   formatData(data) {
     //\u2705: WHITE HEAVY CHECK MARK to represent successful build
     //\u274C: CROSS MARK to represent failed build
@@ -114,6 +108,8 @@ class BuildAF extends React.Component {
     return icon + " ( " + data.result + " ) " + description;
   }
 
+  //@return:
+  //  builder if specified, else "timer"
   formatCauses(causes) {
     if (causes) {
       return causes[0].userId ? causes[0].userId : "timer";
@@ -121,45 +117,74 @@ class BuildAF extends React.Component {
     return "unknown causes beyond our control";
   }
 
+  //@return:
+  //  unicode dots to represent trimmed data
+  displayDots() {
+    return (
+      <ListItem>
+        <ListItemText
+          primary={"\u22EE"} //\u22EE: Vertical Ellipsis to represent trimmed data
+          primaryTypographyProps={{ variant: "h5" }}
+          style={styles.listitemtextdots}
+        ></ListItemText>
+      </ListItem>
+    );
+  }
+
+  //@return:
+  //  display list of build contents (builder [data.causes], build start time, build result, build description)
+  displayListContents(data, index) {
+    return (
+      <ListItem
+        key={index}
+        button
+        component="a"
+        href={this.props.jenkinlink + data.id}
+      >
+        <ListItemText
+          primary={this.formatData(data)}
+          secondary={
+            extractTime(data.startTime) +
+            " Triggered by " +
+            this.formatCauses(data.causes)
+          }
+          primaryTypographyProps={{ variant: "h5" }}
+          secondaryTypographyProps={{ variant: "h6" }}
+          style={styles.listitemtext}
+        ></ListItemText>
+      </ListItem>
+    );
+  }
+
   //return bullet format of failed Data and its content for display
   //if failed data has been trimmed for too many data, it will show ... for trimmed off data.
-  getListContents(maxNum) {
+  //which will results in showing few most recent failed builds + ... + few most oldest failed builds
+  //@param
+  //  maxNum : total number of builds to show
+  //  latestShowNum : number of most recent failed builds to show
+  //@return
+  //  first, returns builds number equal to latestShowNum
+  //  second, if total number of failed data is larger than maxNum to show, returns ... (displaydots function) to represent trimmed data
+  //  third, if there are more remaining data to show, return the remaining data.
+  //  If there are no failed data to show at all, returns no builds detected string
+  getListContents(maxNum, latestShowNum) {
     const { failedData } = this.state;
     if (failedData.length > 0) {
       return (
         <List>
-          {failedData.slice(0, maxNum).map((data, index) => (
-            <ListItem
-              key={index}
-              button
-              component="a"
-              href={this.props.jenkinlink + data.id}
-            >
-              <ListItemText
-                primary={formatData(data)}
-                secondary={
-                  extractTime(data.startTime) +
-                  " Triggered by " +
-                  formatCauses(data.causes)
-                }
-                primaryTypographyProps={{ variant: "h5" }}
-                secondaryTypographyProps={{ variant: "h6" }}
-                style={styles.listitemtext}
-              ></ListItemText>
-            </ListItem>
-          ))}
+          {failedData
+            .slice(0, latestShowNum)
+            .map((data, index) => this.displayListContents(data, index))}
 
-          {failed.length >= maxNum ? (
-            <ListItem key={index}>
-              <ListItemText
-                primary={"\u22EE"} //\u22EE: Vertical Ellipsis to represent trimmed data
-                primaryTypographyProps={{ variant: "h5" }}
-                style={styles.listitemtextdots}
-              ></ListItemText>
-            </ListItem>
-          ) : (
-            undefined
-          )}
+          {failedData.length > maxNum ? this.displayDots() : undefined}
+
+          {failedData.length > maxNum
+            ? failedData
+                .slice(failedData.length - (maxNum - latestShowNum))
+                .map((data, index) => this.displayListContents(data, index))
+            : failedData
+                .slice(latestShowNum)
+                .map((data, index) => this.displayListContents(data, index))}
         </List>
       );
     } else {
@@ -178,9 +203,11 @@ class BuildAF extends React.Component {
 
   render() {
     return this.state.isLoading ? (
-      <Card style={styles.card}>Loading. . .</Card>
+      <Card style={styles.card} raised={true}>
+        Loading. . .
+      </Card>
     ) : (
-      <Card style={styles.card}>
+      <Card style={styles.card} raised={true}>
         <CardHeader
           title={this.props.pipeline}
           subheader="Display failed build from most recent up to the last successful build"
@@ -190,7 +217,7 @@ class BuildAF extends React.Component {
           titleTypographyProps={{ variant: "h4" }}
           subheaderTypographyProps={{ variant: "h6", color: "inherit" }}
         ></CardHeader>
-        {this.getListContents(4)}
+        {this.getListContents(4, 2)}
       </Card>
     );
   }
