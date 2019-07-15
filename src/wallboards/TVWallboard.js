9c2c4d2ef66d09d3efab5fed7c8dd8610cb8cdb2
bankent1 <bankent1@email.arizona.edu>
2019-07-15 13:27:02 -0700

    Removed extra div tag

--- src/wallboards/TVWallboard.js
+++ src/wallboards/TVWallboard.js
@@ -7,19 +7,17 @@ import { RightBox, LeftBox, ContentHorz } from "./WallboardStyles";
 class DefaultWallboard extends React.Component {
   render() {
     return (
-      <div>
-        <ContentHorz>
-          <LeftBox>
-            {/* Left box content */}
-            <BuildStatus />
-          </LeftBox>
-          <RightBox>
-            {/* Right box content */}
-            <SlackComponent />
-            <Github />
-          </RightBox>
-        </ContentHorz>
-      </div>
+      <ContentHorz>
+        <LeftBox>
+          {/* Left box content */}
+          <BuildStatus />
+        </LeftBox>
+        <RightBox>
+          {/* Right box content */}
+          <SlackComponent />
+          <Github />
+        </RightBox>
+      </ContentHorz>
     );
   }
 }
