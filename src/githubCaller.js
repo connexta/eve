f3d72bbcb650fb51bb1311454a72f0a95a33a6ae
Kyle Grady <Kgrady1@email.arizona.edu>
2019-07-15 23:09:56 -0700

    Update src/githubCaller.js
    Co-Authored-By: Travis Banken <35464088+bankent1@users.noreply.github.com>

--- src/githubCaller.js
+++ src/githubCaller.js
@@ -3,7 +3,8 @@ import Octicon, { GitPullRequest } from "@primer/octicons-react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { CX_OFF_WHITE, CX_FONT, CX_GRAY_BLUE } from "./Constants.js";
-import { hour } from "./TimeUtils";
+import { hour } from "./utilities/TimeUtils";
+
 
 const NUMPULLS = 5;
 const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
