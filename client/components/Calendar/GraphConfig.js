d38ee01966e2713731ebdccbdc1844b515d1e55a
Vina Martin <vina.martin@connexta.com>
2019-09-09 13:49:14 -0700

    Update graph config (#100)

--- client/components/Calendar/GraphConfig.js
+++ client/components/Calendar/GraphConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  appId: "53b3ede2-e972-4d80-8e0d-894151b5ba65",
+  appId: "63595ad7-d372-4227-9ede-590410f05977",
   authority: "https://login.microsoftonline.com/organizations/",
   scopes: ["user.read", "calendars.readwrite"]
 };
