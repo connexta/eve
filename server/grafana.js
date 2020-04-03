a098be673d97b758b2fb2b575519aee5cd87d192
Vina Martin <vina.martin@connexta.com>
2020-04-03 13:03:38 -0700

    Wraps grafana calls in try/catch to prevent OOM (#109)

--- server/grafana.js
+++ server/grafana.js
@@ -11,39 +11,43 @@ const auth_header = "Basic " + new Buffer.from(auth_string).toString("base64");
 //use Puppeteer to take screenshot and write it to the respond url.
 module.exports = {
   getScreenshot: async function(prod, url) {
-    const browser = prod
-      ? await puppeteer.launch({
-          executablePath: "/usr/bin/chromium-browser",
-          headless: true,
-          args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
-            "--headless"
-          ],
-          env: {
-            TZ: "America/Phoenix"
-          }
-        })
-      : await puppeteer.launch({
-          defaultViewport: null,
-          headless: false,
-          args: ["--start-maximized", "--disable-dev-shm-usage"],
-          env: {
-            TZ: "America/Phoenix"
-          }
-        });
+    try {
+      const browser = prod
+        ? await puppeteer.launch({
+            executablePath: "/usr/bin/chromium-browser",
+            headless: true,
+            args: [
+              "--no-sandbox",
+              "--disable-setuid-sandbox",
+              "--disable-dev-shm-usage",
+              "--headless"
+            ],
+            env: {
+              TZ: "America/Phoenix"
+            }
+          })
+        : await puppeteer.launch({
+            defaultViewport: null,
+            headless: false,
+            args: ["--start-maximized", "--disable-dev-shm-usage"],
+            env: {
+              TZ: "America/Phoenix"
+            }
+          });
 
-    const width = prod ? 2560 : 0;
-    const height = prod ? 1286 : 0;
+      const width = prod ? 2560 : 0;
+      const height = prod ? 1286 : 0;
 
-    const page = await browser.newPage();
-    await page.setExtraHTTPHeaders({ Authorization: auth_header });
-    await page.setDefaultNavigationTimeout(0);
-    await page.setViewport({ width: width, height: height });
-    await page.goto(url, { waitUntil: "networkidle0" });
-    const screenshotBuffer = await page.screenshot();
-    await browser.close();
-    return screenshotBuffer;
+      const page = await browser.newPage();
+      await page.setExtraHTTPHeaders({ Authorization: auth_header });
+      await page.setDefaultNavigationTimeout(0);
+      await page.setViewport({ width: width, height: height });
+      await page.goto(url, { waitUntil: "networkidle0" });
+      const screenshotBuffer = await page.screenshot();
+      await browser.close();
+      return screenshotBuffer;
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
